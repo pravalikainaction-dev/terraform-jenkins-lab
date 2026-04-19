84dbab8ef5678ce4f0a22f66436d3c97e1c13869
pravalikainaction-dev <pravalika.in.action@gmail.com>
2026-04-19 21:12:12 +0000

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,21 @@
+erraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.31.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "us-east-1"
+
+} 
+resource "aws_instance" "jenkins_ec2" {
+  ami           = "ami-0ec10929233384c7f"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Jenkins-Terraform-Instance"
+  }
+}
